4119626fb07197c009c6c0fa6098c2ad8fe057f8
Kaze Mewno <pkriachu@gmail.com>
2016-06-29 12:33:07 +0800

    Implement upload file and write into database

--- www/upload.php
+++ www/upload.php
@@ -4,8 +4,107 @@
 <?php
 	include("common.php");
 	include("apks.php");
-	htmlheads("Downloads");
+    htmlheads("Customization");
+
+
+    global $page_title, $page_content;
+
+    if ( isset($_POST["upload"]) ) {
+        process_upload();
+    } else {
+        generate_index_page();
+    }
+
+
+
+    function generate_index_page() {
+        global $page_title, $page_content;
+        $page_title = "Create customized packge for online/offline testing";
+        $page_content = 
+            '<form enctype="multipart/form-data" class="form-horizontal" role="form" method="post" action="upload.php">' . 
+            '<div class="form-group">' .
+          	    '<div class="col-sm-10">' .
+        			'<div class="input-group">' .
+        			    '<i class="input-group-addon fa fa-file-code-o"></i>' .
+        			    '<input name="file" class="form-control" type="file" />' .
+        			    '<span class="input-group-addon">.apk</span>' .
+        		    '</div>' .
+        	    '</div>' .
+        	    '<div class="col-sm-2">' .
+        		    '<input class="form-control" type="submit" />' .
+        	    '</div>' .
+            '</div><!-- form-group -->' .
+            '<div class="form-group">' .
+        	    '<label class="control-label col-sm-1" for="type">Type:</label>' .
+        	    '<div class="col-sm-3">' .
+        	    '<select id="type" name="type" class="form-control">' .
+        		    '<option value="local" selected>Local (Offline)</option>' .
+        		    '<option value="remote">Remote (Online)</option>' .
+        	    '</select>' .
+        	    '</div>' .
+            '</div><!-- form-group -->' .
+            '<input name="upload" type="hidden" value="1" />' .
+            '</form>';
+    }
+
+
+
+    function process_upload() {
+        global $page_title, $page_content;
+        $page_title = "Upload Files";
+        $page_content = "";
+
+        $message = "";
+        // Processing file uploading
+        $file_name = "" . time() . rand(100,999) . ".apk";
+        $file_path = "uploads/$file_name";
+
+        if ( move_uploaded_file($_FILES["file"]["tmp_name"], $file_path) ) {
+            //Upload successfully
+            // Set the parameter of the uploaded file
+            $hash = hash_file( "md5", $file_path );
+
+            if ( isset($_POST["uploader"]) )
+                $uploader = $_POST["uploader"];
+            else
+                $uploader = "Guest";
+
+            if ( isset($_POST["type"]) )
+                $type = $_POST["type"];
+            else
+                $type = "local";
+
+            $message .= "Upload successfully.<br />";
+            $message .= "File name: $file_name<br />";
+            $message .= "File hash: $hash<br />";
+            $message .= "Uploader: $uploader<br />";
+            $message .= "Modify type: $type<br />";
+
+            // wtire into database
+            $db = new PDO("sqlite:./codroid.db");
+            $insert = "INSERT INTO uploads (file_name, file_hash, uploader, modify_type, upload_time) ";
+            $insert .= "VALUES ('$file_name', '$hash', '$uploader', '$type', datetime('now'))";
+            $db->exec($insert);
+
+            // handle exception
+            //
+        } else {
+            $message .= "An error occurred.";
+        }
+        $page_content = $message;
+    }
+
+
+
+    function display_content() {
+        global $page_title, $page_content;
+        echo "<h2>" . $page_title . "</h2>";
+        echo $page_content;
+    }
 ?>
+
+
+
 <style>
 a {
 	text-decoration: none;
@@ -22,102 +121,9 @@ a:visited {
 
 <div class="container">
 
-	<?php navbar("downloads"); ?>
+	<?php navbar("customization"); ?>
 
-	<hr/>
-	<ul class="nav nav-pills">
-	<li role="presentation"><a href="#prebuilt">Pre-built</a></li>
-	<li role="presentation"><a href="#customized">Customization</a></li>
-	</ul>
-	<hr/>
-
-	<a name="prebuilt"></a>
-	<h2>Pre-built packages for offline (local) testing</h2>
-
-	<div class="container">
-	<?php
-		for($i = 0; $i < sizeof($apks); $i++) {
-			if($i % 3 == 0) {
-				echo '<div class="row">';
-			}
-			$baseurl = "http://codroid.org/repo";
-			$cat = $apks[$i]["cat"];
-			$id = $apks[$i]["id"];
-			$name = $apks[$i]["name"];
-			$icon = $apks[$i]["icon"];
-			$apk = $apks[$i]["apks"];
-			$pkg = $apks[$i]["repackaged"];
-			$pat = $apks[$i]["patched"];
-			$meta = $apks[$i]["meta"];
-
-			$pkgurl = "Repackaged";
-			$pkgicon = "fa-ban text-danger";
-			if($pkg != FALSE) {
-				$pkgicon = "fa-copy text-primary";
-				$pkgurl = '<a href="'."$baseurl/repackaged/$cat/$pkg".'">Repackaged</a>';
-			}
-
-			$paticon = "fa-ban text-danger";
-			$patinfo = "Patched";
-			if($pat != FALSE && $meta != FALSE) {
-				$paticon = "fa-check-square-o text-success";
-				$patinfo = '<a href="'."$baseurl/patched/$cat/$pat".'">Patched</a> &nbsp; &nbsp; ';
-				$patinfo .= '<span class="fa fa-file-text-o"></span> ';
-				$patinfo .= '<a href="'."$baseurl/meta/$cat/$meta".'">Metadata</a>';
-			}
-?>
-<div class="col-sm-4">
-<div class="media">
-<div class="media-left">
-<img src="<?= "$baseurl/icons/$cat/$icon"; ?>" width="64" height="64"/>
-</div>
-<div class="media-body">
-<strong><?= $name; ?></strong><br/>
-<?= $id; ?><br/>
-<span class="fa fa-cube"></span> <a href="<?= "$baseurl/apks/$cat/$apk"; ?>">Original</a><br/>
-<span class="fa <?= $pkgicon; ?>"></span> <?= $pkgurl; ?><br/>
-<span class="fa <?= $paticon; ?>"></span> <?= $patinfo; ?><br/><br/>
-</div>
-</div>
-</div>
-<?php
-			if($i % 3 == 2) {
-				echo '</div>';
-			}
-		}
-		if($i % 3 != 0) {
-			echo '</div>';
-		}
-	?>
-	</div>
-
-	<a name="customized"></a>
-	<h2>Create customized packge for online/offline testing</h2>
-	<p>Coming soon ...</p>
-
-	<form class="form-horizontal" role="form">
-	<div class="form-group">
-		<div class="col-sm-10">
-			<div class="input-group">
-				<i class="input-group-addon fa fa-file-code-o"></i>
-				<input name="file" class="form-control" type="file" disabled/>
-				<span class="input-group-addon">.apk</span>
-			</div>
-		</div>
-		<div class="col-sm-2">
-			<input class="form-control" type="submit" disabled/>
-		</div>
-	</div><!-- form-group -->
-	<div class="form-group">
-		<label class="control-label col-sm-1" for="type">Type:</label>
-		<div class="col-sm-3">
-		<select id="type" name="type" class="form-control" disabled>
-			<option selected>Local (Offline)</option>
-			<option>Remote (Online)</option>
-		</select>
-		</div>
-	</div><!-- form-group -->
-	</form>
+    <?php display_content(); ?>
 
 	<?php footer(); ?>
 
