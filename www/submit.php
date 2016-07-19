f126aa6c6e5cc7e294d6058d78f8565c6a0f02d6
Kaze Mewno <pkriachu@yahoo.com.tw>
2016-07-19 14:04:59 +0800

    add write_php_ini()
    From: http://stackoverflow.com/questions/5695145/how-to-read-and-write-to-an-ini-file-with-php

--- www/submit.php
+++ www/submit.php
@@ -69,6 +69,7 @@ if(sizeof($row) == 1) {
 
         );
     }
+
     
 #    $record_path = "/var/www/records/" . $package . "-" . $ip . "-" . $id;
 #    $file = fopen($record_path, "w+");
@@ -76,6 +77,42 @@ if(sizeof($row) == 1) {
 #    fprintf($file, "%s\n", $data);
 }
 
+
+
+function write_php_ini($array, $file)
+{
+    $res = array();
+    foreach($array as $key => $val)
+    {
+        if(is_array($val))
+        {
+            $res[] = "[$key]";
+            foreach($val as $skey => $sval) $res[] = "$skey = ".(is_numeric($sval) ? $sval : '"'.$sval.'"');
+        }
+        else $res[] = "$key = ".(is_numeric($val) ? $val : '"'.$val.'"');
+    }
+    safefilerewrite($file, implode("\r\n", $res));
+}
+
+function safefilerewrite($fileName, $dataToSave)
+{    if ($fp = fopen($fileName, 'w'))
+    {
+        $startTime = microtime(TRUE);
+        do
+        {            $canWrite = flock($fp, LOCK_EX);
+           // If lock not obtained sleep for 0 - 100 milliseconds, to avoid collision and CPU load
+           if(!$canWrite) usleep(round(rand(0, 100)*1000));
+        } while ((!$canWrite)and((microtime(TRUE)-$startTime) < 5));
+
+        //file was locked so now we can store information
+        if ($canWrite)
+        {            fwrite($fp, $dataToSave);
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+
+}
 
 
 ?>
